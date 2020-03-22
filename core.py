fffc527ec04a3febdbb0d0b91993ff8bf26bf0bc
QHwan <poter1026@gmail.com>
2020-03-22 19:15:43 +0900

    optimization

--- core.py
+++ core.py
@@ -1,7 +1,11 @@
 from math import acos, asin
 import numpy as np
 from numpy.random import rand, uniform
+from numpy.core.umath import arctan2, sqrt, sin, cos
 from tqdm import tqdm
+from numba import jit, float64
+
+
 
 class Core(object):
     # class builder initiation 
@@ -13,16 +17,19 @@ class Core(object):
         self.dt = dt #timestep
         self.frame = frame #total iterations
         
+
     # Set up initial condition
     def set_initial_condition(self):
         pos_mat3 = np.zeros((self.frame, self.N, 2))
         vel_mat = np.zeros((self.frame, self.N))
+        norm_vel_vec = np.zeros(self.frame) #normalized absolute velocities
 
         pos_mat3[0,:,0] = self.L*rand(self.N) #initial positions x-coordinates
         pos_mat3[0,:,1] = self.L*rand(self.N) #initial poisitions y-coordinates
-        vel_mat[0] = 2*np.pi*rand(self.N) #initial velocities
-        return(pos_mat3, vel_mat)
+        vel_mat[0] = 2*np.pi*rand(self.N)
+        return(pos_mat3, vel_mat, norm_vel_vec)
     
+
     def theta_noise(self):
         #This is a uniform disribution of noise
         return(uniform(-self.eta/2,self.eta/2,self.N))
@@ -42,15 +49,23 @@ class Birds(Core):
 
     def run(self):
         #This function simulates the scheme selected by the user for the flocking mechanism
-        
         #Inputs -- None
         #Outputs -- Positions and velocities of all timesteps of the simulation
-        pos_mat3, vel_mat = self.set_initial_condition()
+        pos_mat3, vel_mat, norm_vel_vec = self.set_initial_condition()
+        norm_vel_vec[0] = self.calc_norm_vel(vel_mat[0])
         for i in tqdm(range(1, self.frame)):
             pos_mat3[i], vel_mat[i] = self.update_metric(pos_mat3[i-1], vel_mat[i-1])
-
-        return(pos_mat3, vel_mat)
+            norm_vel_vec[i] = self.calc_norm_vel(vel_mat[i])
+        return(pos_mat3, vel_mat, norm_vel_vec)
     
+    
+    def calc_norm_vel(self, vel_vec):
+        vel_sum_vec = np.zeros(2)
+        vel_sum_vec[0] += self.v*np.sum(cos(vel_vec))
+        vel_sum_vec[1] += self.v*np.sum(sin(vel_vec))
+        norm_vel = np.linalg.norm(vel_sum_vec)
+        return(norm_vel/self.N/self.v)
+            
 
     def update_metric(self,pos_mat, vel_vec):
         #This method allows us to update the flocking model with the Metric model
@@ -58,40 +73,36 @@ class Birds(Core):
         #the radius selected, self.r.
         #Inputs -- x-coordinates, y-coordinates, trajectories for time = t
         #Outputs -- x-coordinates, y-coordinates, trajectories for time = t + (delta t)
-        avgs = 0*vel_vec
-        avgc = 0*vel_vec
+        avg_sin = 0*vel_vec
+        avg_cos = 0*vel_vec
         
         for j in range(0,self.N):
             #find distances for all particles
-            dist_vec = self.calc_dist(pos_mat, self.L, j)
+            dist_vec = self.calc_dist(pos_mat, j)
             
             #find indicies that are within the radius
-            ngbs_idx_vec = [i for i in range(len(dist_vec)) if dist_vec[i] <= self.r]
-            
-            #find average velocity of those inside the radius
-            sint = 0 
-            cost = 0
-            for k in ngbs_idx_vec:
-                sint = sint + np.sin(vel_vec[k])
-                cost = cost + np.cos(vel_vec[k])
-            if (len(ngbs_idx_vec)==0):
-                #catch yourself if there are no objects within the desired radius
-                avgs[j] = 0
-                avgc[j] = 0
+            ngbs_idx_vec = np.where(dist_vec <= self.r)[0]
+
+            if len(ngbs_idx_vec) == 0:
+                avg_sin[j] = 0
+                avg_cos[j] = 0
             else:
-                avgs[j] = sint/len(ngbs_idx_vec)
-                avgc[j] = cost/len(ngbs_idx_vec)
+                #find average velocity of those inside the radius
+                sint = np.sum(sin(vel_vec[ngbs_idx_vec]))
+                cost = np.sum(cos(vel_vec[ngbs_idx_vec]))
+                avg_sin[j] = sint/len(ngbs_idx_vec)
+                avg_cos[j] = cost/len(ngbs_idx_vec)
                 
         #construct the noise
         noise = self.theta_noise()
         
         #update velocities and positions
-        cosi = (self.ep)*avgc+(1-self.ep)*np.cos(vel_vec)
-        sini = (self.ep)*avgs+(1-self.ep)*np.sin(vel_vec)
-        newvelo = np.arctan2(sini,cosi) 
+        cosi = (self.ep)*avg_cos+(1-self.ep)*np.cos(vel_vec)
+        sini = (self.ep)*avg_sin+(1-self.ep)*np.sin(vel_vec)
+        newvelo = arctan2(sini,cosi) 
         velon = np.mod(newvelo + noise,2*np.pi)
-        pos_mat[:,0] = pos_mat[:,0] + self.dt*self.v*np.cos(velon) 
-        pos_mat[:,1] = pos_mat[:,1] + self.dt*self.v*np.sin(velon)
+        pos_mat[:,0] = pos_mat[:,0] + self.dt*self.v*cos(velon) 
+        pos_mat[:,1] = pos_mat[:,1] + self.dt*self.v*sin(velon)
         
         #Make sure that the positions are not outside the boundary.
         #If so, correct for periodicity
@@ -101,43 +112,34 @@ class Birds(Core):
         return(pos_mat, velon)
        
     
-    def calc_dist(self, pos_mat, L, j):
+    def calc_dist(self, pos_mat, j):
         #find distance of every particle from particle j using periodic boundary conditions
-        posx = pos_mat[:,0]
-        posy = pos_mat[:,1]
-        Dist0 = np.sqrt((posx[j] - posx)**2 + (posy[j] - posy)**2) #regular  
-        Dist1 = np.sqrt((posx[j]  - L - posx)**2 + (posy[j] - posy + L)**2) #topleft
-        Dist2 = np.sqrt((posx[j]  - posx)**2 + (posy[j] - posy + L)**2) #topcenter
-        Dist3 = np.sqrt((posx[j]  + L - posx)**2 + (posy[j] - posy + L)**2) #topright
-        Dist4 = np.sqrt((posx[j]  - L - posx)**2 + (posy[j] - posy)**2) #middleleft
-        Dist5 = np.sqrt((posx[j]  + L - posx)**2 + (posy[j] - posy)**2) #middleright
-        Dist6 = np.sqrt((posx[j]  - L - posx)**2 + (posy[j] - posy - L)**2) #bottomleft
-        Dist7 = np.sqrt((posx[j]  - posx)**2 + (posy[j] - posy - L)**2) #bottomcenter
-        Dist8 = np.sqrt((posx[j]  + L - posx)**2 + (posy[j] - posy - L)**2) #bottomright
-        
-        TD = [Dist0,Dist1,Dist2,Dist3,Dist4,Dist5,Dist6,Dist7,Dist8]
-        
-        return(np.asarray(TD).min(0)) #minimum values for all possible distances
+        ref_pos_vec = pos_mat[j]
+        pbc_pos_mat = self.check_pbc(np.tile(ref_pos_vec, (self.N,1)), pos_mat)
     
-    def check_boundary(self,pos_mat):
-        posx = pos_mat[:,0]
-        posy = pos_mat[:,1]
-        xcordn = [i for i in range(self.N) if posx[i] < 0]
-        xcordp = [i for i in range(self.N) if posx[i] > self.L]
-        ycordn = [i for i in range(self.N) if posy[i] < 0]
-        ycordp = [i for i in range(self.N) if posy[i] > self.L]
-        
-        for j in xcordn:
-            posx[j] = posx[j] + self.L
-       
-        for j in xcordp:
-            posx[j] = posx[j] - self.L
-            
-        for j in ycordn:
-            posy[j] = posy[j] + self.L
-            
-        for j in ycordp:
-            posy[j] = posy[j] - self.L
-           
+        dist_vec = np.zeros(self.N)
+        dist_vec.fill(self.L*10)
+        mask1 = abs(ref_pos_vec[0] - pbc_pos_mat[:,0]) < self.r
+        mask2 = abs(ref_pos_vec[1] - pbc_pos_mat[:,1]) < self.r
+        mask = mask1 & mask2
+        dist_vec[mask] = sqrt((ref_pos_vec[0]-pbc_pos_mat[mask,0])**2 + (ref_pos_vec[1]-pbc_pos_mat[mask,1])**2)
+        return(dist_vec)
+
+
+    def check_pbc(self, ref_pos_mat, pos_mat):    
+        pbc_pos_mat = np.copy(pos_mat)
+        mask1 = ref_pos_mat - pos_mat > self.L/2
+        mask2 = pos_mat - ref_pos_mat > self.L/2
+        pbc_pos_mat[mask1] += self.L
+        pbc_pos_mat[mask2] -= self.L
+        return(pbc_pos_mat)
+
+
+    def check_boundary(self, pos_mat):
+        mask1 = pos_mat < 0
+        mask2 = pos_mat > self.L
+        pos_mat[mask1] += self.L
+        pos_mat[mask2] -= self.L
         return(pos_mat)
-                                      
+
+                                     
--- test.py
+++ test.py
@@ -2,9 +2,18 @@ from scipy import *
 import numpy as np
 import animation
 import core
+import matplotlib.pyplot as plt
 
-Bird = core.Birds(L=5, N=300, eta=.1, r=1, dt=1, v=0.1, frame=100, ep=1)
-pos_mat3, vel_mat = Bird.run()
+eta_vec = np.linspace(0, 5.0, 11)
+eta_vec = [4.0]
+o_vec = np.zeros_like(eta_vec)
+
+for i, eta in enumerate(eta_vec):
+        Bird = core.Birds(L=3.1, N=40, eta=eta, r=1, dt=1, v=0.1, frame=5000, ep=1)
+        pos_mat3, vel_mat, norm_vel_vec = Bird.run()
+
+        o_vec[i] = np.mean(norm_vel_vec[-20:])
+
 
 
 anim = animation.Simulation_Animation(pos_mat3[:,:,0].T, pos_mat3[:,:,1].T, vel_mat.T, Bird)
